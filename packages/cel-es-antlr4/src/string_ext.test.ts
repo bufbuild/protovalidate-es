8c03ae2994adcb180637d0b5fd4eea2e44b74f02
Alfred Fuller <afuller@buf.build>
2023-06-28 14:40:44 -0700

    split out test helpers

---
+++ packages/cel-es-antlr4/src/string_ext.test.ts
@@ -0,0 +1,44 @@
+import { describe, test, expect } from "vitest";
+import {
+  STRINGS_EXT_TEST,
+  STRINGS_FORMAT_TEST_CASES,
+} from "@bufbuild/cel-es-conformance";
+import { CEL_PARSER, newCelEnv } from "./index";
+import {
+  CelError,
+  makeStringExtFuncRegistry,
+  ObjectActivation,
+  NATIVE_ADAPTER,
+} from "@bufbuild/cel-es";
+import { runSimpleTestFile } from "./testing";
+
+describe("Strings Ext Test", () => {
+  runSimpleTestFile(CEL_PARSER, STRINGS_EXT_TEST);
+});
+
+describe("string.format", () => {
+  STRINGS_FORMAT_TEST_CASES.forEach((tc) => {
+    describe(tc.name, () => {
+      const STRINGS_EXT_FUNCS = makeStringExtFuncRegistry(tc.locale);
+      const env = newCelEnv();
+      env.addFuncs(STRINGS_EXT_FUNCS);
+      // Create the input expression from 'format'.format([formatArgs])
+      const input =
+        "'" + tc.format + "'.format([" + (tc.formatArgs ?? "") + "])";
+      test(`${input}`, () => {
+        const parsed = env.parse(input);
+        const plan = env.plan(parsed);
+        const ctx = new ObjectActivation(
+          tc.dynArgs === undefined ? {} : tc.dynArgs,
+          NATIVE_ADAPTER
+        );
+        const result = plan.eval(ctx);
+        if (tc.err) {
+          expect(result).toBeInstanceOf(CelError);
+        } else {
+          expect(result).toStrictEqual(tc.expectedOutput);
+        }
+      });
+    });
+  });
+});
---
+++ packages/cel-es-antlr4/src/testing.ts
@@ -0,0 +1,77 @@
+import { describe, test, expect } from "vitest";
+import {
+  CelEnv,
+  CelError,
+  CelUnknown,
+  makeStringExtFuncRegistry,
+  ObjectActivation,
+  CEL_ADAPTER,
+  EXPR_VAL_ADAPTER,
+  CelParser,
+} from "@bufbuild/cel-es";
+import {
+  SimpleTest,
+  SimpleTestFile,
+  SimpleTestSection,
+} from "@buf/alfus_cel.bufbuild_es/dev/cel/expr/conformance/simple_pb";
+
+const STRINGS_EXT_FUNCS = makeStringExtFuncRegistry();
+
+export function runSimpleTestCase(celParser: CelParser, testCase: SimpleTest) {
+  const env = new CelEnv(testCase.container, celParser);
+  env.addFuncs(STRINGS_EXT_FUNCS);
+  const parsed = env.parse(testCase.expr);
+  const plan = env.plan(parsed);
+  const ctx = new ObjectActivation(testCase.bindings, EXPR_VAL_ADAPTER);
+  const result = plan.eval(ctx);
+  switch (testCase.resultMatcher.case) {
+    case "value":
+      if (result instanceof CelError || result instanceof CelUnknown) {
+        expect(result).toStrictEqual(testCase.resultMatcher.value);
+      } else {
+        const expected = EXPR_VAL_ADAPTER.valToCel(
+          testCase.resultMatcher.value
+        );
+        if (!CEL_ADAPTER.equals(result, expected)) {
+          const actual = EXPR_VAL_ADAPTER.celToValue(result);
+          expect(actual).toStrictEqual(testCase.resultMatcher.value);
+        }
+      }
+      break;
+    case "evalError":
+    case "anyEvalErrors":
+      expect(result).toBeInstanceOf(CelError);
+      break;
+    case undefined:
+      expect(result).toStrictEqual(true);
+      break;
+    default:
+      throw new Error(
+        `Unsupported result case: ${testCase.resultMatcher.case}`
+      );
+  }
+}
+
+export function runSimpleTestSection(
+  celParser: CelParser,
+  section: SimpleTestSection
+) {
+  describe(section.name, () => {
+    section.test.forEach((tc) => {
+      test(tc.name === "" ? tc.expr : tc.name, () => {
+        runSimpleTestCase(celParser, tc);
+      });
+    });
+  });
+}
+
+export function runSimpleTestFile(
+  celParser: CelParser,
+  testFile: SimpleTestFile
+) {
+  describe(testFile.name, () => {
+    testFile.section.forEach((section) => {
+      runSimpleTestSection(celParser, section);
+    });
+  });
+}
