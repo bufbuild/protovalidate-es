0e00384b72a17d171ddfec8ba1ee41e33c50e69f
Alfred Fuller <afuller@buf.build>
2023-06-28 18:49:21 -0700

    Add string_ext test cases for web

---
+++ packages/cel-es-web/src/string_ext.test.ts
@@ -0,0 +1,47 @@
+import { describe, test, expect } from "vitest";
+import {
+  STRINGS_EXT_TEST,
+  STRINGS_FORMAT_TEST_CASES,
+} from "@bufbuild/cel-es-conformance";
+import { loadCelParser } from "./index";
+import {
+  CelEnv,
+  CelError,
+  makeStringExtFuncRegistry,
+  ObjectActivation,
+  NATIVE_ADAPTER,
+} from "@bufbuild/cel-es";
+import { runSimpleTestFile } from "@bufbuild/cel-es-conformance";
+
+const CEL_PARSER = await loadCelParser("tree-sitter-cel.wasm");
+
+describe("Strings Ext Test", () => {
+  runSimpleTestFile(CEL_PARSER, STRINGS_EXT_TEST);
+});
+
+describe("string.format", () => {
+  STRINGS_FORMAT_TEST_CASES.forEach((tc) => {
+    describe(tc.name, () => {
+      const STRINGS_EXT_FUNCS = makeStringExtFuncRegistry(tc.locale);
+      const env = new CelEnv("", CEL_PARSER);
+      env.addFuncs(STRINGS_EXT_FUNCS);
+      // Create the input expression from 'format'.format([formatArgs])
+      const input =
+        "'" + tc.format + "'.format([" + (tc.formatArgs ?? "") + "])";
+      test(`${input}`, () => {
+        const parsed = env.parse(input);
+        const plan = env.plan(parsed);
+        const ctx = new ObjectActivation(
+          tc.dynArgs === undefined ? {} : tc.dynArgs,
+          NATIVE_ADAPTER
+        );
+        const result = plan.eval(ctx);
+        if (tc.err) {
+          expect(result).toBeInstanceOf(CelError);
+        } else {
+          expect(result).toStrictEqual(tc.expectedOutput);
+        }
+      });
+    });
+  });
+});
