6e278a8328c42cb9189e3ccbcf02f615ab0f838a
Alfred Fuller <afuller@buf.build>
2023-06-30 20:15:18 -0700

    Give cel-es-repl-web a facelift

--- packages/cel-es-repl-web/src/App.tsx
+++ packages/cel-es-repl-web/src/App.tsx
@@ -1,89 +1,108 @@
-import { useState } from 'react';
-import './App.css'
-import { CelEnv, CelError, CelResult, CelUnknown, NATIVE_ADAPTER, makeStringExtFuncRegistry } from '@bufbuild/cel-es';
-import { loadCelParser } from '@bufbuild/cel-es-web';
-import { Timestamp } from '@bufbuild/protobuf'
+import { useState } from "react";
+import "./App.css";
+import {
+  CelEnv,
+  CelError,
+  CelResult,
+  CelUnknown,
+  NATIVE_ADAPTER,
+  makeStringExtFuncRegistry,
+} from "@bufbuild/cel-es";
+import { loadCelParser } from "@bufbuild/cel-es-web";
+import { Timestamp } from "@bufbuild/protobuf";
+import yaml from "yaml";
 
 const APP_ENV = new CelEnv();
 APP_ENV.addFuncs(makeStringExtFuncRegistry());
-loadCelParser('tree-sitter-cel.wasm').then(parser => {
+loadCelParser("tree-sitter-cel.wasm").then((parser) => {
   APP_ENV.setParser(parser);
 });
 
 function processLet(env: CelEnv, args: string): CelResult {
-  const [name, expr] = args.split('=');
-  const result = env.run(expr);
+  const [name, expr] = args.split("=");
+  let result;
+  try {
+    result = env.run(expr);
+  } catch (e) {
+    result = new CelError(0, String(e));
+  }
   env.set(name.trim(), result);
   return result;
 }
 
-function processInput(env: CelEnv, input: string): string {
-  env.set('now', Timestamp.now());
-  let result : CelResult;
-  if (input[0] === '%') {
-    const pos = input.indexOf(' ');
-    const command = input.slice(1, pos > 0 ? pos : undefined);
-    const args = pos > 0  ? input.slice(pos + 1): '';
-    switch (command) {
-      case 'let':
-        result = processLet(env, args);
-        break;
-      case 'env': {
-        let output = "\n"
-        for (const [name, value] of Object.entries(env.data)) {
-          output += `${name} = ${value}\n`;
-        }
-        return output;
-      }
-      default:
-        return `Unknown command: ${command}`;
-    }
-  } else {
+function processInput(env: CelEnv, input: string, data: string): string {
+  env.set("now", Timestamp.now());
+  const lines = data.split(";");
+  for (const line of lines) {
+    processLet(env, line);
+  }
+  let result;
+  try {
     result = env.run(input);
+  } catch (e) {
+    result = new CelError(0, String(e));
   }
   if (result instanceof CelError) {
     return `Error: ${result.message}`;
   } else if (result instanceof CelUnknown) {
     return `Unknown: ${result.ids}`;
   }
-  return String(NATIVE_ADAPTER.fromCel(result));
+  const native = NATIVE_ADAPTER.fromCel(result);
+  return yaml.stringify(native, (_, value) => {
+    switch (typeof value) {
+      case "object":
+        if (value instanceof Map) {
+          return Object.fromEntries(value);
+        }
+        return value;
+      default:
+        return value;
+    }
+  });
 }
-
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [messages, setMessages] = useState<
-    {
-      input: string;
-      output: string;
-    }[]
-  >([]);
-  return <>
-    <ol>
-      {messages.map((msg, index) => (
-        <li key={index}>
-          <code>{msg.input}{` => `}{msg.output}</code>
-        </li>
-      ))}
-    </ol>
-    <form onSubmit={async (e) => {
-      e.preventDefault();
-      // Clear inputValue since the user has submitted.
-      setInputValue("");
-      // Store the inputValue in the chain of messages and
-      // mark this message as coming from "me"
-      setMessages((prev) => [
-        ...prev,
-        {
-          input: inputValue,
-          output: processInput(APP_ENV, inputValue)
-        },
-      ]);
-    }}>
-      <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
-      <button type="submit">Send</button>
-    </form>
-  </>;
+  const [inputExpr, setInputExpr] = useState("");
+  const [inputEnv, setInputEnv] = useState("");
+  const [outputValue, setOutputValue] = useState("");
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">cel-es-web</h1>
+        </header>
+        <div className="App-body">
+          <div className="App-left">
+            <textarea
+              className="App-input-textarea"
+              value={inputExpr}
+              onChange={(e) => {
+                setInputExpr(e.target.value);
+                setOutputValue(processInput(APP_ENV, e.target.value, inputEnv));
+              }}
+            />
+          </div>
+          <div className="App-right">
+            <textarea
+              className="App-env-textarea"
+              value={inputEnv}
+              onChange={(e) => {
+                setInputEnv(e.target.value);
+                setOutputValue(
+                  processInput(APP_ENV, inputExpr, e.target.value)
+                );
+              }}
+            />
+            <textarea
+              className="App-output-textarea"
+              value={outputValue}
+              readOnly
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
