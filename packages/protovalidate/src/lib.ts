cbcf3a42d23578453bdbcc9f6d22e327e6b44346
Timo Stamm <ts@timostamm.de>
2025-03-11 20:30:14 +0100

    Add link to the HTML spec next to the email regex (#72)

--- packages/protovalidate/src/lib.ts
+++ packages/protovalidate/src/lib.ts
@@ -575,6 +575,7 @@ function isPort(str: string): boolean {
  * error.
  */
 export function isEmail(str: string): boolean {
+  // See https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
   return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
     str,
   );
