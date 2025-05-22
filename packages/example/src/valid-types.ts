747e1fe1c1a83dde88d104f9c3a0ee874b452946
Timo Stamm <ts@timostamm.de>
2025-05-22 16:40:17 +0200

    Add an example for Valid types

---
+++ packages/example/src/valid-types.ts
@@ -0,0 +1,55 @@
+// Copyright 2024-2025 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { create } from "@bufbuild/protobuf";
+import { createValidator } from "@bufbuild/protovalidate";
+import {
+  OrderSchema,
+  type OrderValid,
+  type User,
+} from "./gen/store/v1/order_pb";
+
+const validator = createValidator({
+  // This option enabled validation of the proto2 `required` label.
+  legacyRequired: true,
+});
+
+const order = create(OrderSchema, {
+  id: "ed1cb800-75cb-4e4c-95ab-e093a7f23e55",
+  user: {
+    name: "John Doe",
+  },
+});
+
+const result = validator.validate(OrderSchema, order);
+
+switch (result.kind) {
+  case "valid":
+    processOrder(result.message);
+    break;
+  default:
+    throw result.error;
+}
+
+function processOrder(order: OrderValid) {
+  console.log(`processing order ${order.id}`);
+  // On the Valid type, the `user` property isn't optional, because
+  // the field is annotated with the protovalidate required rule.
+  setUserActive(order.user);
+  return true;
+}
+
+function setUserActive(user: User) {
+  console.log(`user ${user.name} has ordered something`);
+}
